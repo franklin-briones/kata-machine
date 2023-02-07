feeced7b3938b15290ed1d72ed46007d2203688c
Franklin Briones <franklindbriones@gmail.com>
2023-02-07 15:39:04 -0600

    implement bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,13 @@
 export default function bubble_sort(arr: number[]): void {
 
+    let n = arr.length
+    for (let i = 0; i < n; i++) {
+        for ( let j = 0; i < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                const tmp = arr[j]
+                arr[j] = arr[j -1]
+                arr[j + 1] = tmp
+            }
+        }
+    }
 }
